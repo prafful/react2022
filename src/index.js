b4efbbae3b1b04d92b62ba052998a2b42172c101
Prafful Daga <prraful@gmail.com>
2022-01-25 09:44:12 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FirstComponent from './rootcomponent/root';
+
+
+var location = document.getElementById("firstcomponent")
+
+ReactDOM.render(<FirstComponent></FirstComponent>, location)
---
+++ src/rootcomponent/root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class FirstComponent extends React.Component {
+    //state = {  } 
+    render() { 
+        return (
+            <h1>My Root Component!!!!</h1>
+        );
+    }
+}
+ 
+export default FirstComponent;
